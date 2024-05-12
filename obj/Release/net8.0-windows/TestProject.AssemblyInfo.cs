a3fb6cc8913a108d0a61871ec2b04dccd25578fe
thund3rgrove <alex007613@gmail.com>
2024-05-13 02:13:53 +0800

    Quick fixes to readme

--- obj/Release/net8.0-windows/TestProject.AssemblyInfo.cs
+++ obj/Release/net8.0-windows/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e51ef93bb9e18bd6f2c33c9959135619ecd6af4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dfac8231f4bd4113870452ebed21172f7b156b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
